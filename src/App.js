f3d7421e794e7f656e2c4b63f4b8d99e25b0f17c
elvis <programmerzbb@163.com>
2019-12-30 00:31:47 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './components/header'
+import { GlobalStyle } from './common/base.css'
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Header></Header>
     </div>
   );
